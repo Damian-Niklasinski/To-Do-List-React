ce1df7fa8d8709dd501c71bf7e5e9f735939e60a
Damian Niklasinski <damianiklasinski@wp.pl>
2024-02-20 19:26:41 +0100

    changed styles in buttons and code formatted

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
 
-const Buttons = ( {tasks, hideDoneTasks} ) => (
+const Buttons = ({ tasks, hideDoneTasks }) => (
     <div className="buttons">
         {tasks.length > 0 && (
             <React.Fragment>
-                <button className="buttons__button">
+                <button
+                    className="buttons__button">
                     {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
                 </button>
                 <button
