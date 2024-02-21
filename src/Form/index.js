22d829c36971b0b690913e7087c2b71301677182
Damian Niklasinski <damianiklasinski@wp.pl>
2024-02-21 19:25:47 +0100

    autofocus named changed to autoFocus

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const Form = () => (
     <form className="form__form">
-        <input className="form__formInput" autofocus placeholder="Co jest do zrobienia?" />
+        <input className="form__formInput" autoFocus placeholder="Co jest do zrobienia?" />
         <button className="form__formButton">Dodaj zadanie</button>
     </form>
 );
